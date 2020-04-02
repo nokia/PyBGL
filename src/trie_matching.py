779cdcaa1e235fdb8a1df3cf5189838798a22568
Marc-Olivier Buob <marc-olivier.buob@nokia-bell-labs.com>
2020-04-02 21:13:17 +0200

    Renamed src/matching.py -> src/trie_matching.py, renamed matching -> trie_matching

--- src/trie_matching.py
+++ src/trie_matching.py
@@ -14,23 +14,23 @@ from pybgl.trie import Trie, is_final
 from pybgl.parallel_breadth_first_search import \
     ParallelBreadthFirstSearchVisitor, parallel_breadth_first_search
 
-class MatchingVisitor(ParallelBreadthFirstSearchVisitor):
+class TrieMatchingVisitor(ParallelBreadthFirstSearchVisitor):
     def __init__(self):
         self.counters = [0, 0, 0, 0]
-    def update(self, q1 :int, g1 :Automaton, q2 :int, g2 :Automaton):
+    def update(self, q1 :int, g1 :Trie, q2 :int, g2 :Trie):
         i = 1 * int(is_final(q1, g1)) + 2 * int(is_final(q2, g2))
         self.counters[i] += 1
-    def start_vertex(self, s1 :int, g1 :Automaton, s2 :int, g2 :Automaton):
+    def start_vertex(self, s1 :int, g1 :Trie, s2 :int, g2 :Trie):
         self.update(s1, g1, s2, g2)
-    def discover_vertex(self, s1 :int, g1 :Automaton, s2 :int, g2 :Automaton):
+    def discover_vertex(self, s1 :int, g1 :Trie, s2 :int, g2 :Trie):
         self.update(s1, g1, s2, g2)
 
-def matching(
+def trie_matching(
     g1 :Trie,
     g2 :Trie,
     if_push = lambda e1, g1, e2, g2: True,
-    vis :MatchingVisitor= None
+    vis :TrieMatchingVisitor= None
 ) -> list:
-    if vis is None: vis = MatchingVisitor()
+    if vis is None: vis = TrieMatchingVisitor()
     parallel_breadth_first_search(g1, g2, vis = vis, if_push = if_push)
     return vis.counters
