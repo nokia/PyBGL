329bd2ef9f0c828841c3efed1da7b3412d76fbcd
Marc-Olivier Buob <marc-olivier.buob@nokia-bell-labs.com>
2020-07-29 01:27:27 +0200

    Fixed date

--- src/color.py
+++ src/color.py
@@ -10,7 +10,7 @@
 __author__     = "Marc-Olivier Buob"
 __maintainer__ = "Marc-Olivier Buob"
 __email__      = "marc-olivier.buob@nokia-bell-labs.com"
-__copyright__  = "Copyright (C) 2018, Nokia"
+__copyright__  = "Copyright (C) 2020, Nokia"
 __license__    = "BSD-3"
 
 # Note: this module is complementary to colorsys.
