28a42f436b95db44e37c923b823754788f865aa1
Maxime Raynal <maxime.raynal@nokia.com>
2020-08-07 13:01:15 +0200

    add docstrings

--- src/prune_incidence_automaton.py
+++ src/prune_incidence_automaton.py
@@ -18,12 +18,27 @@ from pybgl.property_map        import make_assoc_property_map
 from pybgl.reverse             import reverse_graph
 
 def find_reachable_vertices(g: Graph, sources: set) -> set:
+    """
+    Returns the set of vertices of a graph which are reachable
+    from a set of source vertices.
+    Args:
+        g: Graph, an instance of `Graph`
+        sources: set, a set of integers representing the source vertices
+    Returns:
+        The set of vertices that are reachable from the source vertices
+    """
     map_vcolor = defaultdict(int)
     pmap_vcolor = make_assoc_property_map(map_vcolor)
     depth_first_search_graph(g, sources, pmap_vcolor=pmap_vcolor)
     return set(map_vcolor.keys())
 
 def prune_incidence_automaton(g: IncidenceAutomaton):
+    """
+    Prunes the vertices of an IncidenceAutomaton that cannot be reached
+    from the intial state, or that cannot reach a final state.
+    Args:
+        g: IncidenceAutomaton, an instance of IncidenceAutomaton
+    """
     to_keep = find_reachable_vertices(g, {initial(g)})
     reverse_graph(g)
     to_keep &= find_reachable_vertices(g, final(g))
