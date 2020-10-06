aa49530c464b7bca019ee0cac1304ba5043e6181
Marc-Olivier Buob <marc-olivier.buob@nokia-bell-labs.com>
2020-10-06 14:43:59 +0200

    Added beside()

--- src/html.py
+++ src/html.py
@@ -22,3 +22,37 @@ def html(s :str):
     chart = HTML(s)
     # or chart = charts.plot(...)
     display(chart)
+
+def beside(contents1 :str, contents2 :str, title :str = "", title1 :str = "", title2 :str = "") -> str:
+    """
+    Export two HTML contents so that they are displayed beside.
+    Args:
+        contents1: A `str` containing the content displayed on the right.
+        contents2: A `str` containing the content displayed on the left.
+        title: A `str` containing the title of the figure.
+        title1: A `str` containing the caption of the left content.
+        title2: A `str` containing the caption of the right content.
+    Returns:
+        The corresponding HTML `str` instance.
+    """
+    return """
+        %(title)s
+        <div style="display: table; width:100%%">
+            <div style="display: table-row">
+                <div style="width: 50%%; padding: 2%%; display: table-cell;">
+                    <b>%(title1)s</b>
+                    <pre>%(contents1)s</pre>
+                </div>
+                <div style="width: 50%%; padding: 2%%; display: table-cell;">
+                    <b>%(title2)s</b>
+                    <pre>%(contents2)s</pre>
+                </div>
+            </div>
+        </div>
+        """ % {
+            "title"     : title,
+            "title1"    : title1,
+            "title2"    : title2,
+            "contents1" : contents1,
+            "contents2" : contents2
+        }
