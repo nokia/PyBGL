140b53f78d4dfde74a81a378e472786f6d0b766b
Marc-Olivier Buob <marc-olivier.buob@nokia-bell-labs.com>
2020-10-20 15:51:05 +0200

    Added nfa.py

---
+++ src/nfa.py
@@ -0,0 +1,197 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+__author__     = "Marc-Olivier Buob"
+__maintainer__ = "Marc-Olivier Buob"
+__email__      = "marc-olivier.buob@nokia-bell-labs.com"
+__copyright__  = "Copyright (C) 2020, Nokia"
+__license__    = "BSD-3"
+
+from pybgl.automaton import *
+from pybgl.property_map import ReadWritePropertyMap
+
+EPSILON = "\u03b5"
+
+class Nfa(DirectedGraph):
+    def __init__(
+        self,
+        num_vertices :int = 0,
+        initials     :set = None,
+        pmap_final   :ReadWritePropertyMap = None,
+        epsilon      :str = EPSILON
+    ):
+        super().__init__(num_vertices)
+        self.initials = initials if initials else {0}
+        if not pmap_final:
+            self.map_final = defaultdict(bool)
+            self.pmap_final = make_assoc_property_map(self.map_final)
+        else:
+            self.pmap_final = pmap_final
+        self.epsilon = epsilon
+
+    def delta_one_step(self, qs, a) -> set:
+        sets = [set(self.adjacencies.get(q, dict()).get(a, dict()).keys()) for q in qs]
+        return set.union(*sets) if sets else set()
+
+    def delta_epsilon(self, qs) -> set:
+        ret = set()
+        qs_new = set(qs)
+        while qs_new:
+            ret |= qs_new
+            qs_new = self.delta_one_step(qs_new, self.epsilon)
+            qs_new -= ret
+        return ret
+
+    def delta(self, q :int, a :chr) -> set:
+        qs = self.delta_epsilon({q})
+        qs = self.delta_one_step(qs, a)
+        qs = self.delta_epsilon(qs)
+        return qs
+
+    def sigma(self, q :int) -> set:
+        qs = self.delta_epsilon({q})
+        return (
+            set() if q is BOTTOM else
+            {
+                a
+                for q in qs
+                for a in self.adjacencies.get(q, dict()).keys()
+                if a != self.epsilon
+            }
+        )
+
+    def add_edge(self, q :int, r :int, a :chr) -> tuple:
+        arn = self.adjacencies.get(q)
+        if arn is None:
+            arn = self.m_adjacencies[q] = dict()
+        rn = arn.get(a)
+        if rn is None:
+            rn = self.m_adjacencies[q][a] = dict()
+        s = rn.get(r)
+        if s is None:
+            s = rn[r] = set()
+        n = len(s) + 1
+        s.add(n)
+        return (EdgeDescriptor(q, r, (a, n)), True)
+
+    def remove_edge(self, e :EdgeDescriptor):
+        q = source(e, self)
+        r = target(e, self)
+        (a, n) = e.m_distinguisher
+        try:
+            del self.m_adjacencies[q][a][r]
+        except KeyError:
+            pass
+
+    def out_edges(self, q :int):
+        return (
+            EdgeDescriptor(q, r, (a, n))
+            for (q, arn) in self.adjacencies.get(q, dict()).items()
+            for (a, rn) in arn.items()
+            for (r, n) in rn.items()
+        )
+
+    def edges(self):
+        return (
+            EdgeDescriptor(q, r, (a, n))
+            for (q, arn) in self.adjacencies.items()
+            for (a, rn) in arn.items()
+            for (r, n) in rn.items()
+        )
+
+    def alphabet(self) -> set:
+        return {
+            a
+            for (q, arn) in self.adjacencies.items()
+            for a in arn.keys()
+            if a != self.epsilon
+        } if q is not None else set()
+
+    def set_initial(self, q :int, is_initial :bool = True):
+        if is_initial:
+            self.initials.add(q)
+        elif q in self.initials:
+            self.initials.remove(q)
+
+    def initials(self) -> set:
+        return self.initials
+
+    def is_initial(self, q :int) -> bool:
+        return q in self.initials
+
+    def set_initial(self, q :int, is_initial :bool = True):
+        if is_initial:
+            self.initials.add(q)
+        else:
+            self.initials.discard(q)
+
+    def set_initials(self, q0s :set):
+        self.initials = {q0 for q0 in q0s}
+
+    def label(self, e :EdgeDescriptor) -> chr:
+        (a, n) = e.m_distinguisher
+        return a
+
+    def set_final(self, q :int, is_final :bool = True):
+        self.pmap_final[q] = is_final
+
+    def is_final(self, q :int) -> bool:
+        return self.pmap_final[q]
+
+    def vertex_to_graphviz(self, q :int) -> str:
+        return "%s [shape=\"%s\"]" % (
+            q,
+            "doublecircle" if self.is_final(q) else "circle"
+        )
+
+    def edge_to_graphviz(self, e :EdgeDescriptor) -> str:
+        l = label(e, self)
+        if l == self.epsilon:
+            l = "<i>\u03b5</i>"
+        return "%s %s %s [label=<%s>]" % (
+            source(e, self),
+            graphviz_arc(self),
+            target(e, self),
+            l
+        )
+
+    def to_dot(self, graphviz_style :str = None) -> str:
+        if graphviz_style == None:
+            graphviz_style = default_graphviz_style()
+        return "%(type)s G {  %(style)s  %(vertices)s%(sep1)s  %(edges)s%(sep2)s\n}" % {
+            "style"    : graphviz_style,
+            "type"     : graphviz_type(self),
+            "vertices" : ";\n  ".join(["  %s" % self.vertex_to_graphviz(u) for u in vertices(self)]),
+            "sep1"     : ";" if num_vertices(self) else "",
+            "edges"    : ";\n  ".join(["  %s" % self.edge_to_graphviz(e)   for e in edges(self)]),
+            "sep2"     : ";" if num_edges(self) else "",
+        }
+
+    def accepts(self, w) -> True:
+        return any(is_final(q, self) for q in delta_word(w, self))
+
+    def delta_word(self, w) -> set:
+        qs = set(initials(self))
+        qs = self.delta_epsilon(qs)
+        for a in w:
+            if not qs: break
+            qs = set.union(*[delta(q, a, self) for q in qs])
+        return qs
+
+    def finals(self):
+        return (q for q in vertices(self) if is_final(q, self))
+
+def epsilon(nfa :Nfa) -> chr:
+    return nfa.epsilon
+
+def is_epsilon_transition(e :EdgeDescriptor, nfa :Nfa) -> bool:
+    return label(e, nfa) == epsilon(nfa)
+
+def initials(nfa :Nfa):
+    return (q for q in nfa.initials)
+
+def set_initials(q0s, nfa :Nfa):
+    nfa.set_initials(q0s)
+
+def delta_word(w, nfa) -> set: # Overloads Automaton.delta_word
+    return nfa.delta_word(w)
---
+++ tests/test_nfa.py
@@ -0,0 +1,104 @@
+#!/usr/bin/env pytest-3
+# -*- coding: utf-8 -*-
+
+__author__     = "Marc-Olivier Buob"
+__maintainer__ = "Marc-Olivier Buob"
+__email__      = "marc-olivier.buob@nokia-bell-labs.com"
+__copyright__  = "Copyright (C) 2018, Nokia"
+__license__    = "BSD-3"
+
+from pybgl.nfa import *
+
+def make_nfa1(eps = EPSILON):
+    g = Nfa(4)
+    (e01a, _) = add_edge(0, 1, "a", g)
+    assert label(e01a, g) == "a"
+    assert is_epsilon_transition(e01a, g) == False
+    (e02a, _) = add_edge(0, 2, "a", g)
+    (e02b, _) = add_edge(0, 2, "b", g)
+    (e02a, _) = add_edge(0, 2, "a", g)
+    (e21b, _) = add_edge(2, 1, "a", g)
+    (e03e, _) = add_edge(0, 3, eps, g)
+    assert label(e03e, g) == epsilon(g)
+    assert is_epsilon_transition(e03e, g) == True
+    (e33c, _) = add_edge(3, 3, "c", g)
+    set_final(1, g)
+    set_final(3, g)
+    assert set(vertices(g)) == {0, 1, 2, 3}
+    return g
+
+def test_num_vertices():
+    g = make_nfa1()
+    assert num_vertices(g) == 4
+
+def test_num_edges():
+    g = make_nfa1()
+    assert num_edges(g) == 6
+
+def test_sigma():
+    # sigma(q, g) list symbols != epsilon carried by egress transition
+    # starting from q or any state reachable from q via one or more
+    # epsilon transitions.
+    g = make_nfa1()
+    assert sigma(0, g) == {"a", "b", "c"}
+    assert sigma(1, g) == set()
+    assert sigma(2, g) == {"a"}
+    assert sigma(3, g) == {"c"}
+
+def test_accepts():
+    g = make_nfa1()
+    assert accepts("a", g) == True
+    assert accepts("aa", g) == True
+    assert accepts("b", g) == False
+    assert accepts("ba", g) == True
+    assert accepts("c", g) == True
+    assert accepts("cc", g) == True
+    assert accepts("", g) == True
+
+def test_finals():
+    g = make_nfa1()
+    f = set(finals(g))
+    assert f == {1, 3}
+    for q in vertices(g):
+        assert is_final(q, g) == bool(q in f)
+
+def test_initials():
+    g = make_nfa1()
+    i = set(initials(g))
+    assert i == {0}
+    for q in vertices(g):
+        assert is_initial(q, g) == bool(q in i)
+
+def test_delta():
+    g = make_nfa1()
+    assert delta(0, "a", g) == {1, 2}
+    assert delta(0, "b", g) == {2}
+    assert delta(0, "c", g) == {3}
+    assert delta(1, "a", g) == set()
+    assert delta(1, "b", g) == set()
+    assert delta(1, "c", g) == set()
+    assert delta(2, "a", g) == {1}
+    assert delta(2, "b", g) == set()
+    assert delta(2, "c", g) == set()
+    assert delta(3, "a", g) == set()
+    assert delta(3, "b", g) == set()
+    assert delta(3, "c", g) == {3}
+
+def test_delta_word():
+    g = make_nfa1()
+    assert delta_word("a", g) == {1, 2}
+    assert delta_word("aa", g) == {1}
+    assert delta_word("z", g) == set()
+    assert delta_word("ccc", g) == {3}
+
+def test_set_initials():
+    g = make_nfa1()
+    assert set(initials(g)) == {0}
+    set_initial(2, g, True)
+    assert accepts("a", g) == True
+    assert accepts("aa", g) == True
+    assert accepts("ccc", g) == True
+    set_initial(0, g, False)
+    assert accepts("a", g) == True
+    assert accepts("aa", g) == False
+    assert accepts("ccc", g) == False
