259c7e604134c8eb9ecae3b5867f5534d37b62ef
Marc-Olivier Buob <marc-olivier.buob@nokia-bell-labs.com>
2020-10-20 16:07:53 +0200

    Fixed typo shutting_yard_postfix -> shunting_yard_postfix

--- tests/test_shunting_yard_postfix.py
+++ tests/test_shunting_yard_postfix.py
@@ -7,14 +7,14 @@ __email__      = "marc-olivier.buob@nokia-bell-labs.com"
 __copyright__  = "Copyright (C) 2020, Nokia"
 __license__    = "BSD-3"
 
-from pybgl.shutting_yard_postfix import OPERATORS_ALG, OPERATORS_RE, shunting_yard_postfix
+from pybgl.shunting_yard_postfix import OPERATORS_ALG, OPERATORS_RE, shunting_yard_postfix
 
-def test_shutting_yard_postfix_alg():
+def test_shunting_yard_postfix_alg():
     assert list(shunting_yard_postfix("2+3*4+(5-6)/7", OPERATORS_ALG)) == [
         '2', '3', '4', '*', '+', '5', '6', '-', '7', '/', '+'
     ]
 
-def testshutting_yard_postfix_re():
+def test_shunting_yard_postfix_re():
     assert list(shunting_yard_postfix("((1.1)?.2.2.2)*?.(3.3)+.(4.4)", OPERATORS_RE)) == [
         '1', '1', '.', '?', '2', '.', '2', '.', '2', '.', '*', '?', '3', '3', '.', '+',
         '.', '4', '4', '.', '.'
