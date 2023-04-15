44d99889842ad3523febc922fe44cd012bc9ac06
Marc-Olivier Buob <marc-olivier.buob@nokia-bell-labs.com>
2023-04-15 20:18:29 +0200

    Removed ipynb_get_*ground_color

--- tests/test_ipynb.py
+++ tests/test_ipynb.py
@@ -7,23 +7,11 @@ __email__      = "marc-olivier.buob@nokia-bell-labs.com"
 __copyright__  = "Copyright (C) 2020, Nokia"
 __license__    = "BSD-3"
 
-from pybgl.ipynb import in_ipynb, ipynb_get_background_color, ipynb_get_foreground_color
+from pybgl.ipynb import in_ipynb
 
 if not in_ipynb():
     def test_in_ipynb():
         assert in_ipynb() is False
-
-    def test_ipynb_get_background_color():
-        assert ipynb_get_background_color() is None
-
-    def test_ipynb_get_foreground_color():
-        assert ipynb_get_background_color() is None
 else:
     def test_in_ipynb():
         assert in_ipynb() is True
-
-    def test_ipynb_get_background_color():
-        assert isinstance(ipynb_get_background_color(), str)
-
-    def test_ipynb_get_foreground_color():
-        assert isinstance(ipynb_get_background_color(), str)
